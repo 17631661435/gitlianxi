49f19500a25d5ddb0d3de848bdee8ab9987a830c
吕帅齐 <2088532997@qq.com>
2022-09-04 12:21:51 +0000

    update src/main/java/com/aa/aa.java.
    Signed-off-by: 吕帅齐 <2088532997@qq.com>

--- src/main/java/com/aa/aa.java
+++ src/main/java/com/aa/aa.java
@@ -3,5 +3,7 @@ package com.aa;
 public class aa {
     public static void main(String[] args) {
         System.out.println("aaaaaaaa");
+            System.out.println("aaaaaaaa");
+                System.out.println("aaaaaaaa");
     }
 }
